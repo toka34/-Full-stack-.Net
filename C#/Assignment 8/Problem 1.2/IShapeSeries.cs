72daebcfde2881670334cc2290671e17be63449d
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-19 00:35:21 +0200

    Add files via upload

---
+++ C#/Assignment 8/Problem 1.2/IShapeSeries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public interface IShapeSeries
+    {
+        int CurrentShapeArea { get; set; }
+        void GetNextArea();
+        void ResetSeries();
+    }
+}
---
+++ C#/Assignment 8/Problem 1.2/ShapeSeries.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public class SquareSeries : IShapeSeries
+    {
+        private int currentSideLength;
+        public int CurrentShapeArea { get; set; }
+
+        public SquareSeries()
+        {
+            ResetSeries();
+        }
+
+        public void GetNextArea()
+        {
+            currentSideLength++;
+            CurrentShapeArea = currentSideLength * currentSideLength;
+        }
+
+        public void ResetSeries()
+        {
+            currentSideLength = 0;
+            CurrentShapeArea = 0;
+        }
+    }
+
+    public class CircleSeries : IShapeSeries
+    {
+        private int currentRadius;
+        public int CurrentShapeArea { get; set; }
+
+        public CircleSeries()
+        {
+            ResetSeries();
+        }
+
+        public void GetNextArea()
+        {
+            currentRadius++;
+            CurrentShapeArea = (int)(Math.PI * currentRadius * currentRadius);
+        }
+
+        public void ResetSeries()
+        {
+            currentRadius = 0;
+            CurrentShapeArea = 0;
+        }
+    }
+}
