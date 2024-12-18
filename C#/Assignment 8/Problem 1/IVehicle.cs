f24883e4c031671e76d2086d9fcbec5b0dd822fe
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-19 00:30:10 +0200

    Add files via upload

---
+++ C#/Assignment 8/Problem 1/IVehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public interface IVehicle
+    {
+        void StartEngine();
+        void StopEngine();
+    }
+}
---
+++ C#/Assignment 8/Problem 1/car1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public class car1 : IVehicle
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Car engine started.");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Car engine stopped.");
+        }
+    }
+    public class Bike : IVehicle
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Bike engine started.");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Bike engine stopped.");
+        }
+    }
+}
