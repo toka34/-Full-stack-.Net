c2a2a1137bf544525be50d7767097ce14eb6c1ae
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-19 00:39:01 +0200

    Add files via upload

---
+++ C#/Assignment 8/Problem 8/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public class Book
+    {
+        // Attributes
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        // Default constructor
+        public Book()
+        {
+            Title = "Unknown Title";
+            Author = "Unknown Author";
+        }
+
+        // Constructor that takes only Title
+        public Book(string title)
+        {
+            Title = title;
+            Author = "Unknown Author";
+        }
+
+        // Constructor that takes both Title and Author
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        // Override ToString for easy display
+        public override string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}";
+        }
+    }
+}
