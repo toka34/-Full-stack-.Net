be92cb0e42044ea0eac522f480d45fdf5cfdf287
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-19 00:38:37 +0200

    Add files via upload

---
+++ C#/Assignment 8/Problem 7/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public interface ILogger
+    {
+        // Default implementation of Log method
+        void Log()
+        {
+            Console.WriteLine("Default log message from ILogger.");
+        }
+    }
+}
