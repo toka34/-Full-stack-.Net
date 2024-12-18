07741cf38424c844e55798745600614d8f8ad3c6
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-19 00:37:42 +0200

    Add files via upload

---
+++ C#/Assignment 8/Problem 5/Robot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_08
+{
+    public class Robot : IWalkable
+    {
+        // This method is specific to the Robot class
+        public void Walk()
+        {
+            Console.WriteLine("Robot is walking in a straight line.");
+        }
+
+        // Explicit interface implementation
+        void IWalkable.Walk()
+        {
+            Console.WriteLine("Robot is walking with a robotic gait.");
+        }
+
+        // Additional method specific to Robot
+        public void Charge()
+        {
+            Console.WriteLine("Robot is charging.");
+        }
+    }
+}
