ec52168bbac6450c020d55a34fd87a87f5f98905
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-22 21:18:28 +0200

    Add files via upload

---
+++ C#/Assignment 9/Problem 15.1/CircleClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.problem_15
+{
+    public class CircleClass
+    {
+        public double Radius { get; set; }
+        public string Color { get; set; }
+
+        public CircleClass(double radius, string color)
+        {
+            Radius = radius;
+            Color = color;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is CircleClass circle)
+            {
+                return Radius == circle.Radius && Color == circle.Color;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Radius, Color).GetHashCode();
+        }
+    }
+}
