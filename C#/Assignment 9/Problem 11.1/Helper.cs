d6822f09177d337a0be733cf573e1d59a6d56a55
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-22 21:13:41 +0200

    Add files via upload

---
+++ C#/Assignment 9/Problem 11.1/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.Problem_11
+{
+    public static class Helper
+    {
+        // Generic method to return the maximum of two values
+        public static T Max<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
