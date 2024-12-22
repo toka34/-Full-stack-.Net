e5ae99c224672249ff2327856f925db94d954e90
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-22 21:16:18 +0200

    Add files via upload

---
+++ C#/Assignment 9/Problem 14.1/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.Problem_14
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
