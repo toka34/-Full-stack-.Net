7cf0772cc52d2c5265cab45e9a157003c780f329
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-22 21:12:28 +0200

    Add files via upload

---
+++ C#/Assignment 9/Problem 10.1/Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Day_09.Problem_14;
+
+namespace Day_09.Problem_10
+{
+    public struct Employee // Change internal to public
+    {
+        // Attributes
+        private int EmpID;
+        private string EmpName;
+        private decimal empsalary;
+        public Department Department { get; set; }
+
+        // Properties
+        public string GetName()
+        {
+            return EmpName;
+        }
+
+        public void SetName(string value)
+        {
+            EmpName = value.Length <= 20 ? value : value.Substring(0, 20);
+        }
+
+        public decimal Salary
+        {
+            get { return empsalary; }
+            set { empsalary = value < 4000 ? 4000 : value; }
+        }
+
+        public int Age { get; set; }
+
+        // Constructor
+        public Employee(int _id, string _Name, decimal _salary, int _Age, Department department)
+        {
+            EmpID = _id;
+            EmpName = _Name;
+            empsalary = _salary;
+            Age = _Age;
+            Department = department;
+        }
+
+        // Override ToString
+        public override string ToString()
+        {
+            return $"Emp Id: {EmpID}, Name: {EmpName}, Salary: {empsalary}, Age: {Age}, Department: {Department}";
+        }
+
+        // Override Equals
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee employee)
+            {
+                return EmpID == employee.EmpID; // Compare based on EmpID
+            }
+            return false;
+        }
+
+        // Override GetHashCode
+        public override int GetHashCode()
+        {
+            return EmpID.GetHashCode(); // Use EmpID for hash code
+        }
+
+        // Bonus property
+        public decimal Bonus
+        {
+            get { return empsalary * 0.1M; }
+        }
+    }
+}
---
+++ C#/Assignment 9/Problem 10.1/Helper2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Day_09.Problem_14;
+
+namespace Day_09.Problem_10
+{
+    public static class Helper2
+    {
+        #region Problem 10
+        public static Employee? SearchArray(Employee[] employees, Employee target)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.Equals(target))
+                {
+                    return employee; // Return the found employee
+                }
+            }
+            return null; // Return null if not found
+        }
+        #endregion
+
+        #region Problem 14
+        // Method to search for employees by department
+        public static Employee[] SearchByDepartment(Employee[] employees, Department department)
+        {
+            Employee[] matchingEmployees = new Employee[employees.Length];
+            int count = 0;
+
+            foreach (var employee in employees)
+            {
+                if (employee.Department.Equals(department))
+                {
+                    matchingEmployees[count] = employee;
+                    count++;
+                }
+            }
+
+            // Resize the array to the number of matches found
+            Array.Resize(ref matchingEmployees, count);
+            return matchingEmployees;
+        } 
+        #endregion
+
+        #region Problem 12
+        // Generic method to replace all occurrences of a specified value in an array
+        public static void ReplaceArray<T>(T[] array, T oldValue, T newValue)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(oldValue))
+                {
+                    array[i] = newValue;
+                }
+            }
+        } 
+        #endregion
+    }
+}
