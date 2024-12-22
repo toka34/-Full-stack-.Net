eeaaa8b994baf2423a3be6afdb73cbbd9c63739f
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-22 21:15:13 +0200

    Add files via upload

---
+++ C#/Assignment 9/Problem 13.1/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.Problem_13
+{
+    public struct Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override string ToString()
+        {
+            return $"Rectangle [Length: {Length}, Width: {Width}]";
+        }
+    }
+}
---
+++ C#/Assignment 9/Problem 13.1/RectangleHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.Problem_13
+{
+    public static class RectangleHelper
+    {
+        public static void Swap(ref Rectangle rect1, ref Rectangle rect2)
+        {
+            Rectangle temp = rect1;
+            rect1 = rect2;
+            rect2 = temp;
+        }
+    }
+}
