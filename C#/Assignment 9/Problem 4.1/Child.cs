728b26066448a47d7a8242542f95b05b12c5cc30
toka34 <161936588+toka34@users.noreply.github.com>
2024-12-22 21:07:25 +0200

    Add files via upload

---
+++ C#/Assignment 9/Problem 4.1/Child.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.Problem_4
+{
+    public class Child : Parent
+    {
+        // Constructor
+        public Child(decimal salary) : base(salary)
+        {
+        }
+
+        // Method to display the salary
+        public void DisplaySalary()
+        {
+            Console.WriteLine($"The salary is: {Salary}");
+        }
+    }
+}
---
+++ C#/Assignment 9/Problem 4.1/parent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_09.Problem_4
+{
+    public class Parent
+    {
+        // Non-virtual Salary property
+        public decimal Salary { get; private set; } // Private setter to prevent modification
+
+        // Constructor
+        public Parent(decimal salary)
+        {
+            Salary = salary;
+        }
+    }
+}
